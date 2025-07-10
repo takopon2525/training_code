728e92598c23317aaf0779c15527888e2b3f0c2a
takopon <doluche.avalanche@gmail.com>
2025-07-10 10:24:29 +0900

    # FizzBuzzゲーム

--- hello.rb
+++ hello.rb
@@ -1,17 +1,4 @@
-def fizzbuzz(max_number)
-     (1..max_number).each do |number|
-        if number % 3 == 0 && number % 5 == 0
-            puts "FizzBuzz"
-        elsif number % 3 == 0
-            puts "Fizz"
-        elsif number % 5 == 0
-            puts "Buzz"
-        else
-            puts number
-        end
-    end
-end
-
+# FizzBuzzゲーム
 # Example usage
 fizzbuzz(15)
 
